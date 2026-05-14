3c6ae84000f243b360ff88fdbd01b4929b3ea7b6
Joonas Teller <jteller@hkhk.edu.ee>
2026-05-14 09:41:04 +0300

    töö

---
+++ h04.2.php
@@ -0,0 +1,50 @@
+<<<<<<< HEAD
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Armin on alkohoolik</title>
+</head>
+<body>
+    <h1>harjutus 04</h1>
+    <form action="" method="get">
+        Aasta: <input type="number" name="aasta"><br>
+        <input type="submit" value="Leia juubel">
+    </form>
+    <?php
+        if (!empty($_GET['aasta'])) {
+            $aasta = $_GET['aasta']
+            $vanus = 2026 - $aasta
+            if ($vanus % 5  == 0) {
+                echo "Sul on juubeliaasta<br>";
+            }
+        }
+    ?>
+</body>
+=======
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Armin on alkohoolik</title>
+</head>
+<body>
+    <h1>harjutus 04</h1>
+    <form action="" method="get">
+        Aasta: <input type="number" name="aasta"><br>
+        <input type="submit" value="Leia juubel">
+    </form>
+    <?php
+        if (!empty($_GET['aasta'])) {
+            $aasta = $_GET['aasta']
+            $vanus = 2026 - $aasta
+            if ($vanus % 5  == 0) {
+                echo "Sul on juubeliaasta<br>";
+            }
+        }
+    ?>
+</body>
+>>>>>>> ab9176a2d51cd6ba83c47cc92012b16b705c97c5
+</html>
---
+++ h04.php
@@ -0,0 +1,31 @@
+<<<<<<< HEAD
+=======
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <form action="">
+            Lisa punkti: <input type="text" name="punktid" >
+            <input type="submit" value="hinda"><br>
+        </form>
+        <?php 
+            if (!empty($_GET['punktid']) && is_numeric($_GET['$punktid'])) {
+            $punktid = $_GET['punktid'];
+                switch($punktid){ 
+	            case ($punktid >= 10): echo 'Arv on liiga väike'; 
+	            break; 
+	            case ($punktid >= 5 && $punktid <= 9): echo 'TEHTUD!'; 
+	            break; 
+                case ($punktid < 5 && $punktid >= 0): echo 'KASIN!'; 
+	            break; 
+	            default: echo 'SISESTA OMA PUNKTID!'; 
+            }
+        } 
+    ?>
+</body>
+</html>
+>>>>>>> ab9176a2d51cd6ba83c47cc92012b16b705c97c5
